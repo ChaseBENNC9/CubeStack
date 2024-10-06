0118c48a85b1fe9658819f29b5b2b1faf221b5d6
ChaseBENNC9 <bennc9@student.op.ac.nz>
2024-10-06 23:07:53 +1300

    ADD: Powerup base class

---
+++ Assets/Scripts/Powerups/Powerup.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+/// <summary>
+/// Base class for all powerups
+/// </summary>
+public abstract class Powerup : MonoBehaviour
+{
+    public float cooldown;
+    public Image foreGroundIcon;
+    public Image backgroundIcon;
+    public bool usable = true;
+    public bool active = false;
+
+
+
+    protected abstract void ActivatePowerup();
+
+    public void PowerupPressed()
+    {
+        if (usable)
+        {
+            ActivatePowerup();
+        }
+    }
+
+    protected void BeginCooldown()
+    {
+        foreGroundIcon.fillAmount = 0;
+        usable = false;
+        StartCoroutine(CooldownAnimation());
+    }
+
+    private IEnumerator CooldownAnimation()
+    {
+        float elapsedTime = 0.0f;
+
+        while (elapsedTime < cooldown)
+        {
+            foreGroundIcon.fillAmount = elapsedTime / cooldown;
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        foreGroundIcon.fillAmount = 1;
+        usable = true;
+    }
+
+
+
+
+}
