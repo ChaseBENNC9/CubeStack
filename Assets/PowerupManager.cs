d51cb869337d7ba509bbdcc4c8e05f1f1a3c6c19
ChaseBENNC9 <bennc9@student.op.ac.nz>
2024-10-08 17:54:30 +1300

    ADD: Powerup manager

---
+++ Assets/PowerupManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class PowerupManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private List<Powerup> powerups;
+    void Start()
+    {
+        powerups = GameObject.FindObjectsOfType<Powerup>().ToList<Powerup>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (Powerup powerup in powerups)
+        {
+            powerup.UpdateButton();
+        }
+    }
+}
